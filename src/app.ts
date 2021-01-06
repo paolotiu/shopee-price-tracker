078a403bd345ecc8f7217da66f852d49368d14de
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2021-01-06 17:08:27 +0800

    change error log

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .catch((e) => console.log(e));
+  .catch((e) => console.log("MongoDB Failed to connect"));
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
 
