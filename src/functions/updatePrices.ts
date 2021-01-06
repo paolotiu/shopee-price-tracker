42fc7101b3029c37d9a31b4aaf4d23eb6ed5e77f
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2021-01-06 17:02:14 +0800

    added cron

--- src/functions/updatePrices.ts
+++ src/functions/updatePrices.ts
@@ -1,25 +1,46 @@
 import Item from "../models/Item";
 import { Item as I } from "../types/Item";
-
+import cron from "node-cron";
 import axios from "axios";
 
 export const updatePrices = async () => {
   const items = await Item.find().exec();
   try {
-    items.forEach(async (item) => {
-      const url = item.api_url;
-      const { data } = await axios.get<I.RootObject>(url);
-      const price = data.item.price_max / 10 ** 5;
-      item
-        .updateOne({
-          price: price,
-          $push: { all_prices: { price: price, time: new Date() } },
-        })
-        .exec();
-    });
+    await Promise.all(
+      items.map(async (item) => {
+        const url = item.api_url;
+        const { data } = await axios.get<I.RootObject>(url);
+        const price = data.item.price_max / 10 ** 5;
+
+        item
+          .updateOne({
+            price: price,
+            $push: { all_prices: { price: price, time: new Date() } },
+          })
+          .exec();
+
+        item
+          .updateOne({
+            price: price,
+            $push: { all_prices: { price: price, time: new Date() } },
+          })
+          .exec();
+      })
+    );
+
+    return { status: "updated" };
   } catch (err) {
     return { status: "error", message: err.message };
   }
-
-  return { status: "updated" };
 };
+let count = 0;
+
+//Cron Job
+const task = cron.schedule(" */15 * * * * *", async () => {
+  const res = await updatePrices();
+  console.log("Updated");
+  if (res.status === "error") {
+    console.log(res.message);
+    task.destroy();
+  }
+});
