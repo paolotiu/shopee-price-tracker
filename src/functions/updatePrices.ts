57e328cb9898b5b6a2e88e5b529b2966fbae717d
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2021-01-06 17:15:28 +0800

    wrapper db search in a try block

--- src/functions/updatePrices.ts
+++ src/functions/updatePrices.ts
@@ -4,8 +4,9 @@ import cron from "node-cron";
 import axios from "axios";
 
 export const updatePrices = async () => {
-  const items = await Item.find().exec();
   try {
+    const items = await Item.find().exec();
+
     await Promise.all(
       items.map(async (item) => {
         const url = item.api_url;
@@ -33,14 +34,15 @@ export const updatePrices = async () => {
     return { status: "error", message: err.message };
   }
 };
-let count = 0;
 
 //Cron Job
-const task = cron.schedule(" */15 * * * * *", async () => {
+const task = cron.schedule(" */15 * * * *", async () => {
   const res = await updatePrices();
-  console.log("Updated");
   if (res.status === "error") {
+    console.log("\x1b[31m%s\x1b[0m", "Destroying update task");
     console.log(res.message);
     task.destroy();
+  } else {
+    console.log("\x1b[32m%s\x1b[0m", "Updated");
   }
 });
