09f65a5e255e0272e2e90ee3cab12ed535192802
Gabriel Paolo Tiu <paolotiu17@gmail.com>
2021-01-08 22:24:59 +0800

    created isAuth middleware

---
+++ src/functions/util.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+
+export const isAuth: RequestHandler = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  return next(createHttpError(401, "Unauthorized"));
+};
