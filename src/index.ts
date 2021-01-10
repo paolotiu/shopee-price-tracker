6c7b1801df2e886424aa8a76a2530d7b9c67a471
Paolo Tiu <paolotiu17@gmail.com>
2021-01-10 12:24:01 +0800

    changes start script

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
 #!/usr/bin/env node
 
 // Import deps
-import app from "./app";
-const debug = require("debug")("server");
-import http from "http";
+import app from './app';
+const debug = require('debug')('server');
+import http from 'http';
 
 // Set the port
-const port = process.env.PORT || "3001";
-app.set("port", port);
+const port = process.env.PORT || '3001';
+app.set('port', port);
 
 // Create http server
 const server = http.createServer(app);
 
 // Listen
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Event listener for HTTP server "error" event.
  */
 
 function onError(error: NodeJS.ErrnoException) {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -49,6 +49,6 @@ function onError(error: NodeJS.ErrnoException) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr?.port;
-  debug("Listening on " + bind);
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+  debug('Listening on ' + bind);
 }
