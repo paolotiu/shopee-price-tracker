2f1f8d4b7faf89e4b6c97689206b9c09974bd8b5
Paolo Tiu <paolotiu17@gmail.com>
2021-01-10 15:55:57 +0800

    styling

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,21 +1,21 @@
-import { PassportStatic } from "passport";
-import PassportLocal from "passport-local";
-import bcrypt from "bcryptjs";
-import User from "../models/User";
+import { PassportStatic } from 'passport';
+import PassportLocal from 'passport-local';
+import bcrypt from 'bcryptjs';
+import User from '../models/User';
 
-export function passportConfig(passport: PassportStatic) {
+export default function passportConfig(passport: PassportStatic) {
   passport.use(
     new PassportLocal.Strategy((username, password, done) => {
       User.findOne({ username: username }).exec((err, user) => {
         if (err) return done(err);
         if (!user) {
-          return done(null, false, { message: "Incorrect username." });
+          return done(null, false, { message: 'Incorrect username.' });
         }
 
         const isValid = bcrypt.compareSync(password, user.password);
 
         if (!isValid) {
-          return done(null, false, { message: "Incorrect password." });
+          return done(null, false, { message: 'Incorrect password.' });
         }
 
         return done(null, user);
