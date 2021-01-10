ba1f90ccd93ddf79b48349d126e9c4f2c48f0bf3
Paolo Tiu <paolotiu17@gmail.com>
2021-01-10 11:58:20 +0800

    added custom message on error passport

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import cookieParser from "cookie-parser";
-import express, { NextFunction, Request, Response } from "express";
-import createError from "http-errors";
-import logger from "morgan";
-import dotenv from "dotenv";
-import session from "express-session";
-import passport from "passport";
-import Connect from "connect-mongo";
-import mongoose from "mongoose";
+import cookieParser from 'cookie-parser';
+import express, { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import passport from 'passport';
+import Connect from 'connect-mongo';
+import mongoose from 'mongoose';
 const MongoStore = Connect(session);
 
 //Make console include timestamp
-require("console-stamp")(console, { pattern: "[HH:MM:ss.l]" });
+require('console-stamp')(console, { pattern: '[HH:MM:ss.l]' });
 
 // Set env files
 dotenv.config();
@@ -19,19 +19,19 @@ dotenv.config();
 const app = express();
 
 // Setup mongoDB connection
-import "./config/mongoDB";
+import './config/mongoDB';
 
 // Setup middlewares
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    secret: "mysecret",
+    secret: 'mysecret',
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
     }),
-    name: "spt-jar",
+    name: 'spt-jar',
     cookie: { maxAge: 1000 * 60 * 60 },
   })
 );
@@ -40,37 +40,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Setup passport
-import { passportConfig } from "./config/passport";
+import { passportConfig } from './config/passport';
 passportConfig(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Setup routes
-import IndexRouter from "./routes";
+import IndexRouter from './routes';
 
-app.use("/", IndexRouter);
+app.use('/', IndexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404, "Endpoint not found"));
+  next(createError(404, 'Endpoint not found'));
 });
 
 // error handler
-app.use(
-  (
-    err: createError.HttpError,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // return error message
-    res.status(err.status || 500);
-    res.json(err.message);
+app.use((err: createError.HttpError, req: Request, res: Response, next: NextFunction) => {
+  if (err.name === 'AuthenticationError' && err.message === 'Bad Request') {
+    return res.status(403).json('No username or password provided');
   }
-);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // return error message
+  res.status(err.status || 500);
+  res.json(err.message);
+});
 
 export default app;
