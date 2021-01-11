d955e5f664bba15f8bf351a9f6e101d085e4f2dc
Paolo Tiu <paolotiu17@gmail.com>
2021-01-11 22:43:32 +0800

    added arg for heroku pupeteer to work

--- server/src/functions/scraper.ts
+++ server/src/functions/scraper.ts
@@ -1,19 +1,19 @@
-import puppeteer from "puppeteer";
-import axios from "axios";
-import { Item } from "../types/Item";
+import puppeteer from 'puppeteer';
+import axios from 'axios';
+import { Item } from '../types/Item';
 export const scrape = async (link: string): Promise<Item.RootObject> => {
-  if (!link || !link.search("shopee.ph")) {
-    throw new Error("Not a valid link");
+  if (!link || !link.search('shopee.ph')) {
+    throw new Error('Not a valid link');
   }
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   let result: Item.RootObject | undefined | null;
   let closed = false;
   // Enable to get website request
   await page.setRequestInterception(true);
 
-  page.on("request", async (request) => {
-    if (request.url().includes("shopee.ph/api/v2/item/get")) {
+  page.on('request', async (request) => {
+    if (request.url().includes('shopee.ph/api/v2/item/get')) {
       const url = request.url();
       const { data } = await axios.get<Item.RootObject>(url);
       result = data;
@@ -33,7 +33,7 @@ export const scrape = async (link: string): Promise<Item.RootObject> => {
 
   await page
     .goto(link, {
-      waitUntil: "networkidle2",
+      waitUntil: 'networkidle2',
     })
     .catch((e) => {});
 
@@ -42,9 +42,9 @@ export const scrape = async (link: string): Promise<Item.RootObject> => {
   }
 
   if (result === undefined) {
-    throw new Error("Error in scraping");
+    throw new Error('Error in scraping');
   } else if (result === null) {
-    throw new Error("No item found");
+    throw new Error('No item found');
   }
   return result;
 };
