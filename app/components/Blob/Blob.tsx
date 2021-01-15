0e3c2700419eea7b613c0e81d2e9a669c9dd2123
Paolo Tiu <paolotiu17@gmail.com>
2021-01-15 16:09:51 +0800

    added color transition

--- app/components/Blob/Blob.tsx
+++ app/components/Blob/Blob.tsx
@@ -9,7 +9,7 @@ const blobs = [
   <Blob1 className="blob  top-0 right-0 hidden md:block md:max-h-96 lg:max-h-70vh z--1" />,
   <Blob2 className="blob md:w-auto w-full top-0 left-0 hidden lg:max-h-70vh md:block md:max-h-96 z--1" />,
   <Blob3 className="blob bottom-0 left-0 hidden md:block z--1" />,
-  <Blob4 className="w-full blob  top-0 max-h-25vh md:hidden z--1" />,
+  <Blob4 className="w-full blob  top-0 md:hidden z--1" />,
   <Blob5 className="blob bottom-0 left-0 md:hidden z--1" />,
 ];
 
