882c7690af3398215e4d951d21eaca0a3d90d9d2
Paolo Tiu <paolotiu17@gmail.com>
2021-01-16 23:15:12 +0800

    api stuff

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -2,9 +2,47 @@ import axios from "axios";
 const BASE_URL = "http://localhost:3001";
 
 export const login = async (email: string, password: string) => {
-  const res = await axios.post(BASE_URL + "/user/login", {
+  const res = await axios.post(
+    BASE_URL + "/user/login",
+    {
+      email,
+      password,
+    },
+    {
+      withCredentials: true,
+    }
+  );
+  console.log(res);
+};
+
+export const signUp = async (email: string, password: string) => {
+  const res = await axios.post(BASE_URL + "/user/sign-up", {
     email,
     password,
+    callbackUrl: "http://localhost:3000/confirmation/",
+  });
+
+  console.log(res.data);
+};
+
+export const confirmEmail = async (token: string) => {
+  const res = await axios.get(BASE_URL + "/confirmation/" + token);
+
+  console.log(res.data);
+  return res.data;
+};
+
+export const logOut = async () => {
+  const res = await axios.get(BASE_URL + "/user/logout", {
+    withCredentials: true,
+  });
+
+  return res.data;
+};
+
+export const getUser = async () => {
+  const res = await axios.get(BASE_URL + "/user/items", {
+    withCredentials: true,
   });
   console.log(res);
 };
---
+++ app/utils/apiHandler.ts
@@ -0,0 +1,7 @@
+export const apiHandler = async <T>(promise: Promise<T>) => {
+  return promise
+    .then((data) => ({ data: data, error: undefined }))
+    .catch((error) =>
+      Promise.resolve({ data: undefined, error: error.response.data })
+    );
+};
