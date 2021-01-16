c092860904bb4a73ff6d89bafe1b8d4878bd100e
Paolo Tiu <paolotiu17@gmail.com>
2021-01-16 17:11:33 +0800

    added logout

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -9,6 +9,13 @@ const router = Router();
 
 router.post('/sign-up', signUpUser);
 router.post('/login', loginUser);
+router.get('/logout', (req, res, next) => {
+  res.clearCookie('spt-jar');
+  req.session.destroy((err) => {
+    if (err) return next(err);
+    return res.json({ message: 'Successfully logged out' });
+  });
+});
 router.get('/items', checkItems);
 router.post('/resendEmail', resendConfirmationEmail);
 export { router as userRouter };
