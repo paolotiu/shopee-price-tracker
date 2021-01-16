20e3878865b5cc12c302e9e962c871308b7e4ff4
Paolo Tiu <paolotiu17@gmail.com>
2021-01-16 23:14:09 +0800

    Made navbar integrated

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,21 +1,25 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
+import { Navbar } from "./General/Navbar";
 
 type Props = {
   children?: ReactNode;
   title?: string;
   className?: string;
+  showNavbar?: boolean;
 };
 
 const Layout = ({
   children,
   title = "This is the default title",
+  showNavbar = true,
   ...props
 }: Props) => (
   <div {...props}>
     <Head>
       <title> {title}</title>
     </Head>
+    {showNavbar && <Navbar />}
     {children}
   </div>
 );
