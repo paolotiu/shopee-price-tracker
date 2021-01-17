6a0cac69c48c6dd9aeae7b342fc4ce0b89a39c96
Paolo Tiu <paolotiu17@gmail.com>
2021-01-17 17:22:41 +0800

    added fallback error

--- app/utils/apiHandler.ts
+++ app/utils/apiHandler.ts
@@ -2,6 +2,6 @@ export const apiHandler = async <T>(promise: Promise<T>) => {
   return promise
     .then((data) => ({ data: data, error: undefined }))
     .catch((error) =>
-      Promise.resolve({ data: undefined, error: error.response.data })
+      Promise.resolve({ data: undefined, error: error.response?.data || error })
     );
 };
