39e6709473a893794ca9ffff37a113a8e267a775
Paolo Tiu <paolotiu17@gmail.com>
2021-01-21 21:08:40 +0800

    added button logout

--- app/components/General/Sidebar.tsx
+++ app/components/General/Sidebar.tsx
@@ -4,12 +4,13 @@ import { logOut } from "../../utils/api";
 
 export const Sidebar = () => {
   return (
-    <div className="hidden h-full p-6 text-xl transition duration-1000 md:flex-col md:justify-between md:flex w-72 bg-black-lighter">
+    <div className="hidden h-full p-6 text-xl text-white md:flex-col md:justify-between md:flex w-72 bg-black-lighter">
       <div className="flex flex-col">
         <span>Home</span>
         <span>Account</span>
       </div>
       <button
+        className="max-w-min"
         onClick={async () => {
           await logOut();
           Router.push("/");
