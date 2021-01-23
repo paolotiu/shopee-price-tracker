71a0eaf3739e80c805d5cb4c23788a627676412a
Paolo Tiu <paolotiu17@gmail.com>
2021-01-23 23:42:20 +0800

    heroku

--- server/src/app.ts
+++ server/src/app.ts
@@ -38,7 +38,7 @@ app.use(
     }),
     name: 'spt-jar',
     // TEMP
-    cookie: { maxAge: 1000 * 60 * 60 * 24, secure: true },
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
   })
 );
 app.use(cookieParser());
