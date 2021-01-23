87900d95f6fe3243de0fc54ae4812b518a58f970
Paolo Tiu <paolotiu17@gmail.com>
2021-01-23 20:06:01 +0800

    Revert "added oauth buttons"
    This reverts commit cb1180b467dea0b210632f546b2e7e25faa444be.

--- app/pages/signup.tsx
+++ app/pages/signup.tsx
@@ -10,10 +10,6 @@ import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { addUser } from "../slices/userSlice";
 import { useRouter } from "next/router";
-import {
-  FacebookLoginButton,
-  GoogleLoginButton,
-} from "react-social-login-buttons";
 interface Fields {
   email: string;
   password: string;
@@ -75,8 +71,8 @@ const SignUp = () => {
   };
   return (
     <Layout showLogin={false} title="Sign Up">
-      <div className="flex items-center justify-center mt-20 overflow-hidden">
-        <div className="flex flex-col w-screen max-w-md px-4 py-8 transition duration-1000 bg-white border-2 rounded-lg shadow dark:bg-black dark:border-yellow-600 border-accent sm:px-6 md:px-8 sm:max-w-md lg:px-10 max-w-90vw">
+      <div className="flex justify-center mt-32">
+        <div className="flex flex-col max-w-md px-4 py-8 mb-40 transition duration-1000 bg-white border-2 rounded-lg shadow dark:bg-black dark:border-yellow-600 border-accent sm:px-6 md:px-8 lg:px-10">
           <div className="self-center mb-2 text-xl font-light text-gray-800 transition duration-1000 sm:text-2xl dark:text-white">
             Create a new account
           </div>
@@ -86,47 +82,6 @@ const SignUp = () => {
               <a className="underline-yellow hover:text-primary"> Sign In</a>
             </Link>
           </span>
-          <div className="grid gap-5 py-8 pb-5 text-center">
-            <a href="http://localhost:3001/auth/facebook" className="w-full">
-              <button
-                type="button"
-                className="flex items-center justify-center w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-              >
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="mr-2"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z"></path>
-                </svg>
-                Continue with Facebook
-              </button>
-            </a>
-            <a href="http://localhost:3001/auth/google">
-              <button
-                type="button"
-                className="flex items-center justify-center w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-red-600 rounded-lg shadow-md hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-              >
-                <svg
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="mr-2"
-                  viewBox="0 0 1792 1792"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z"></path>
-                </svg>
-                Continue with Google
-              </button>
-            </a>
-          </div>
-          <div className="w-full h-3 text-center text-gray-300 border-b opacity-70">
-            <span className="inline-block px-1 bg-black">OR</span>
-          </div>
           <Formik
             initialValues={{ email: "", password: "" }}
             validationSchema={schema}
@@ -158,7 +113,7 @@ const SignUp = () => {
             {({ isSubmitting }) => {
               return (
                 <Form>
-                  <div className="mt-6 ">
+                  <div className="p-6 mt-6">
                     <div className="flex flex-col mb-2">
                       <div className="relative ">
                         <Field
