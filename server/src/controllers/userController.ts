92713a0b0a13f8eeadf799b8c6a24b4a63fc4a18
Paolo Tiu <paolotiu17@gmail.com>
2021-01-25 21:42:13 +0800

    Added checkoneItem

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -2,22 +2,54 @@ import { RequestHandler } from 'express';
 import User from '../models/User';
 
 import { isAuth } from '../functions/util';
+import createHttpError from 'http-errors';
+import { IItem } from '../models/Item';
 
 // Return all items tracked by user
 export const checkItems: RequestHandler[] = [
   isAuth,
   async (req, res, next) => {
-    const user = await User.findById(req.user?._id)
+    const { items } = await User.findById(req.user?._id, 'items -_id')
       .populate({
         path: 'items',
         populate: {
           path: 'item',
         },
       })
-      .lean()
+      .lean<{ items: { item: IItem }[] }>()
       .exec();
 
-    return res.json(user?.items);
+    return res.json(items);
+  },
+];
+
+// Return one item by user
+
+export const checkOneItem: RequestHandler[] = [
+  isAuth,
+  async (req, res, next) => {
+    const { id } = req.params;
+    if (!id) {
+      return next(createHttpError(400, 'No id provided'));
+    }
+
+    const { items } = await User.findById(req.user?._id, 'items -_id')
+      .populate({
+        path: 'items',
+        populate: {
+          path: 'item',
+        },
+      })
+      .lean<{ items: { item: IItem }[] }>()
+      .exec();
+    if (!items) {
+      return next(createHttpError(400, 'No item matched'));
+    }
+    const item = items.filter(({ item }) => item.itemID === id)[0];
+    if (!item) {
+      return next(createHttpError(400, 'No item matched!'));
+    }
+    return res.json(item);
   },
 ];
 
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,7 +1,8 @@
-import { checkItems, getUser } from './../controllers/userController';
+import { checkItems, checkOneItem, getUser } from './../controllers/userController';
 import { Router } from 'express';
 const router = Router();
 
 router.get('/items', checkItems);
 router.get('/', getUser);
+router.get('/item/:id', checkOneItem);
 export { router as userRouter };
