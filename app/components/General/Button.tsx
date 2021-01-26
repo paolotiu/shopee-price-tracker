5189c2b7d91a517615185ef12365d43ff0371623
Paolo Tiu <paolotiu17@gmail.com>
2021-01-26 18:44:48 +0800

    Addded accent type

--- app/components/General/Button.tsx
+++ app/components/General/Button.tsx
@@ -2,12 +2,35 @@ import React from "react";
 
 interface Props {
   className?: string;
+  type?: "button" | "submit" | "reset" | undefined;
+  accent?: boolean;
+  filled?: boolean;
   onClick?: () => void;
 }
 
-export const Button: React.FC<Props> = ({ children, className, ...props }) => {
+export const Button: React.FC<Props> = ({
+  children,
+  className,
+  accent,
+  filled,
+  ...props
+}) => {
+  if (accent) {
+    return (
+      <button
+        className={
+          filled
+            ? "bg-accent dark:bg-accent-dark py-3 px-4 rounded-full font-bold text-white transition-colors duration-1000"
+            : "border-2 box-border border-accent dark:border-accent-dark text-accent dark:text-accent-dark py-3 px-4 rounded-full font-bold transition-colors duration-1000"
+        }
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
   return (
-    <button className={className ? className : "btn-primary"} {...props}>
+    <button className={"btn-primary " + className} {...props}>
       {children}
     </button>
   );
