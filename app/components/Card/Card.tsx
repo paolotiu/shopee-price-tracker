71f5eab6aea7b19186cc8751375a3bd0e2f24684
Paolo Tiu <paolotiu17@gmail.com>
2021-01-26 18:44:40 +0800

    Change clamp lib

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Clamp from "react-multiline-clamp";
 import { Detail } from "./Detail";
+import ClampLines from "react-clamp-lines";
 
 interface Props {
   title: string;
@@ -23,14 +24,27 @@ export const Card = ({
   return (
     <div className="flex flex-col justify-between w-full h-full max-w-lg transition duration-1000 p-7 dark:bg-white bg-white-pure ">
       <div>
-        <Clamp lines={2}>
+        {/* <ClampLines lines={2}>
           <h1 className="pb-1 font-bold text-black-lighter ">{title}</h1>
-        </Clamp>
+        </ClampLines> */}
+        <ClampLines
+          text={title}
+          id="title-"
+          lines={3}
+          innerElement="h1"
+          className="pb-1 font-bold text-black-lighter"
+        />
         <hr />
+        {/* <ClampLines
+          text={desc}
+          id="desc"
+          lines={6}
+          buttons={false}
+          innerElement="p"
+          className="pt-2 text-sm text-gray-400 pb-7"
+        /> */}
         <p className="pt-2 overflow-hidden text-sm text-gray-400 pb-7">
-          <Clamp lines={3} maxLines={3}>
-            {desc}
-          </Clamp>
+          <Clamp lines={3}>{desc}</Clamp>
         </p>
       </div>
       <div className="grid grid-flow-col auto-cols-fr -mb-7 -mx-7 bg-primary top-full">
