5a6bfee19a6ab3941805cb8ee8029611f669799e
Paolo Tiu <paolotiu17@gmail.com>
2021-01-26 22:59:49 +0800

    added showbottomblob option

--- app/components/MainContent/MainContent.tsx
+++ app/components/MainContent/MainContent.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Background } from "../Background/Background";
 
-export const MainContent: React.FC = ({ children }) => {
+interface Props {
+  showBottomBlob?: boolean;
+}
+export const MainContent: React.FC<Props> = ({ children, showBottomBlob }) => {
   return (
     <>
       <main className="grid grid-flow-col mt-28 auto-cols-auto ">
-        <Background />
+        <Background showBottomBlob={showBottomBlob} />
         <div>{children}</div>
       </main>
     </>
