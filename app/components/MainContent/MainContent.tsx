6d42ea4c0c30a6e26419c8f5253beb157e06a912
Paolo Tiu <paolotiu17@gmail.com>
2021-01-27 21:58:57 +0800

    added option to turn off bg

--- app/components/MainContent/MainContent.tsx
+++ app/components/MainContent/MainContent.tsx
@@ -3,12 +3,17 @@ import { Background } from "../Background/Background";
 
 interface Props {
   showBottomBlob?: boolean;
+  showBackground?: boolean;
 }
-export const MainContent: React.FC<Props> = ({ children, showBottomBlob }) => {
+export const MainContent: React.FC<Props> = ({
+  children,
+  showBottomBlob,
+  showBackground = true,
+}) => {
   return (
     <>
       <main className="grid grid-flow-col mt-28 auto-cols-auto ">
-        <Background showBottomBlob={showBottomBlob} />
+        {showBackground && <Background showBottomBlob={showBottomBlob} />}
         <div>{children}</div>
       </main>
     </>
