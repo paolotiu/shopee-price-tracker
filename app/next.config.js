3ea7a3b2367c8a495c52da8ffa26a0bae77babfb
Paolo Tiu <paolotiu17@gmail.com>
2021-01-31 11:51:38 +0800

    removed bad config

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,7 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
+
 module.exports = withBundleAnalyzer({
   webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
     config.module.rules.push({
