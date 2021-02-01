30ff2d6553389ea10297b90f9fb101090838d230
Paolo Tiu <paolotiu17@gmail.com>
2021-02-01 22:37:31 +0800

    removed sidebar gesture

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,10 +1,6 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
 import { NavProps } from "components/General/Navbar";
-import { motion } from "framer-motion/dist/framer-motion";
-import { PanHandler } from "framer-motion/types/gestures/PanSession";
-import { useDispatch, useSelector } from "react-redux";
-import { directSidebar, sidebarPossibilitySelector } from "../slices/uiSlice";
 import dynamic from "next/dynamic";
 const Navbar = dynamic(() => import("components/General/Navbar"));
 interface Props extends NavProps {
@@ -26,31 +22,8 @@ const Layout = ({
   marginTop = true,
   ...props
 }: Props) => {
-  const dispatch = useDispatch();
-  const isSidebarPossible = useSelector(sidebarPossibilitySelector);
-  const panHandler: PanHandler = (e, info) => {
-    // Prevent opening sidebar when it shoulnd't be there
-    if (!isSidebarPossible) {
-      return;
-    }
-    // Prevent opening sidebar when a pan is detected on the toggleswitch for the theme
-    if (
-      (e.target as Element).classList.contains("cancel-sb-pan") ||
-      (e.target as Element).tagName === "svg" ||
-      (e.target as Element).tagName === "NAV" ||
-      (e.target as Element).tagName === "path"
-    ) {
-      return;
-    }
-
-    if (info.offset.x > 0) {
-      dispatch(directSidebar(true));
-    } else {
-      dispatch(directSidebar(false));
-    }
-  };
   return (
-    <motion.div {...props} onPan={panHandler}>
+    <div {...props}>
       <Head>
         <title> {title}</title>
       </Head>
@@ -64,7 +37,7 @@ const Layout = ({
         )}
         <div>{children}</div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
