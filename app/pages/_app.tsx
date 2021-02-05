c2be95ea8987892681af24581a07febcf6d88601
Paolo Tiu <paolotiu17@gmail.com>
2021-02-05 22:44:46 +0800

    progress bar

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,16 +1,28 @@
-import "../styles/core.css";
-import "../styles/components.css";
-import "../styles/utilities.css";
-import "../styles/custom.css";
+import "styles/core.css";
+import "styles/components.css";
+import "styles/utilities.css";
+import "styles/custom.css";
+import "styles/nprogress.css";
 import React from "react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "react-query";
+import dynamic from "next/dynamic";
 
+// Redux store
 import store from "../store";
+
+// GLobal Styles
 import GlobalStylesComponent from "../components/GlobalStyles";
 
+// Progress bar
+const TopProgressBar = dynamic(
+  () => {
+    return import("components/General/TopProgressBar");
+  },
+  { ssr: false }
+);
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => {
@@ -18,6 +30,7 @@ const App = ({ Component, pageProps }: AppProps) => {
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <GlobalStylesComponent />
+        <TopProgressBar />
         <Toaster />
         <Component {...pageProps} />
       </QueryClientProvider>
