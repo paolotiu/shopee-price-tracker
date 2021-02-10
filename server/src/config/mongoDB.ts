68a851ba639f00a3a3cc59a407ff728a068a9e82
Paolo Tiu <paolotiu17@gmail.com>
2021-02-10 16:05:15 +0800

    added test check

--- server/src/config/mongoDB.ts
+++ server/src/config/mongoDB.ts
@@ -1,14 +1,20 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
-const DB_URL = process.env.DB_URL;
-mongoose
-  .connect(DB_URL as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .catch((e) => console.log('MongoDB Failed to connect'));
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+(() => {
+  if (process.env.NODE_ENV === 'test') {
+    return;
+  }
+  dotenv.config();
+  const DB_URL = process.env.DB_URL;
+  mongoose
+    .connect(DB_URL as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    })
+    .catch((e) => console.log('MongoDB Failed to connect'));
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+})();
