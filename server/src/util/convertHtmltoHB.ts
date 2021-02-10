6efe293d56af315f3b701977391118bd943523d9
Paolo Tiu <paolotiu17@gmail.com>
2021-02-10 16:19:09 +0800

    file pathing correction

--- server/src/util/convertHtmltoHB.ts
+++ server/src/util/convertHtmltoHB.ts
@@ -2,10 +2,9 @@ import { promises as fs } from 'fs';
 import handlebars from 'handlebars';
 import p from 'path';
 const convert = async (path: string, replacements: { [key: string]: string }) => {
-  const html = await fs.readFile(p.join(process.cwd(), path), { encoding: 'utf-8' });
+  const html = await fs.readFile(p.join(__dirname, '../' + path), { encoding: 'utf-8' });
   const template = handlebars.compile(html);
   const converted = template(replacements);
   return converted;
 };
-
 export default convert;
